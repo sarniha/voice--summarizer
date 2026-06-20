e4e58aab62d38760baff6fef7df18e15c0d6e222
unknown <sarniha12@gmail.com>
2026-06-20 13:53:26 +0530

    fixing deployment issues

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://voice-summarizer-five.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
 
     allow_methods=["*"],
