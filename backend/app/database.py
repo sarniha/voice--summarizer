4cc5b36f659df59fa6f7a414095b56481c4fa7f5
unknown <sarniha12@gmail.com>
2026-06-20 14:49:04 +0530

    deployment problem fix

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,8 +6,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL", "").strip()  # ← strip() removes newlines
 
-engine = create_engine(os.getenv("DATABASE_URL"))
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
